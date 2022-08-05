95be8b2496de03e025bf83c5645cc7f7acbf3d3b
Peyman <85022016+Mihankhahp@users.noreply.github.com>
2022-08-05 23:11:53 +0430

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react"
 import './App.css';
+import UseContextHook from "./Hooks/useContext";
 import UseEffectHook from "./Hooks/useEffect";
+import UseImperativehandleHook from "./Hooks/useImperativehandle";
+import UseLayoutEffectHook from "./Hooks/useLayoutEffect";
 import UseReducerHook from "./Hooks/useReducer";
+import UseRefHook from "./Hooks/useRef";
 import UseStateHook from './Hooks/useState';
 
 function App() {
@@ -12,7 +16,17 @@ function App() {
       <UseReducerHook />
       <br />
       <UseEffectHook />
+      <br />
+      <UseRefHook />
+      <br />
+      <UseLayoutEffectHook />
+      <br />
+      <UseImperativehandleHook />
+      <br />
+      <UseContextHook />
     </div>
+
+
   );
 }
 
